af17853b150b9c701a0b5f74e28d2cd91cb26d71
bramos013 <brunoramos013@gmail.com>
2022-01-23 05:04:16 -0300

    Subindo estrutura sonarqube

---
+++ sonarqube/provision.sh
@@ -0,0 +1,24 @@
+#/usr/bin/bash
+useradd sonar
+yum install -y wget java-11-openjdk-devel unzip 
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+unzip sonarqube-9.1.0.47736.zip -d /opt/
+mv /opt/sonarqube-9.1.0.47736 /opt/sonarqube
+chown -R sonar:sonar /opt/sonarqube
+touch /etc/systemd/system/sonar.service
+echo > /etc/systemd/system/sonar.service
+cat <<EOT >> /etc/systemd/system/sonar.service
+[Unit]
+Description=SonarQube service
+After=syslog.target network.target 
+[Service]
+Type=forking
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+User=sonar
+Group=sonar
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOT
+service sonar start
